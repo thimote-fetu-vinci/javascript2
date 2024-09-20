4e96d3822782dbb5b6b1b9e7e0521dd7e0d3f4d2
thimote-fetu-vinci <thimote.fetu@student.vinci.be>
2024-09-20 10:28:43 +0200

    Tutorial finished

--- tutorials/back/api/basic/app.ts
+++ tutorials/back/api/basic/app.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { ErrorRequestHandler } from "express";
 
 import usersRouter from "./routes/users";
 import pizzaRouter from "./routes/pizzas";
+import drinksRouter from "./routes/drinks";
 
 const app = express();
 
@@ -10,5 +11,13 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/users", usersRouter);
 app.use("/pizzas", pizzaRouter);
+app.use("/drinks", drinksRouter);
+
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+    console.error(err.stack);
+    return res.status(500).send("Something broke!");
+};
+
+app.use(errorHandler);
 
 export default app;
