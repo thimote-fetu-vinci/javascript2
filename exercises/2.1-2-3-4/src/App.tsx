59c872bb1d5ec0facf8967c1803d832751f14013
thimote-fetu-vinci <lelemmingadorable@gmail.com>
2024-10-17 20:08:24 +0200

    new:ex2.3

--- exercises/2.1-2-3-4/src/App.tsx
+++ exercises/2.1-2-3-4/src/App.tsx
@@ -5,8 +5,7 @@ interface Movie {
 
 interface CinemaProps {
 	name: string;
-	movie1: Movie;
-	movie2: Movie;
+	movies: Movie[];
 }
 
 const Cinema = (props: CinemaProps) => {
@@ -14,8 +13,11 @@ const Cinema = (props: CinemaProps) => {
 		<div>
 			<h2>{props.name}</h2>
 			<ul>
-				<li><strong>{props.movie1.title}</strong> - Réalisateur : {props.movie1.director}</li>
-				<li><strong>{props.movie2.title}</strong> - Réalisateur : {props.movie2.director}</li>
+				{props.movies.map((movie) => (
+					<li key={movie.title}>
+						<strong>{movie.title}</strong> - {movie.director}
+					</li>
+				))}
 			</ul>
 		</div>
 	);
@@ -34,35 +36,56 @@ const App = () => {
 
 	const cinema1Name = "UGC DeBrouckère";
 
-	const movie1 = {
-		title: "HAIKYU-THE DUMPSTER BATTLE",
-		director: "Susumu Mitsunaka",
-	};
-	const movie2 = {
-		title: "GOODBYE JULIA ",
-		director: "Mohamed Kordofani",
-	};
+	const moviesCinema1 = [
+		{
+			title: "HAIKYU-THE DUMPSTER BATTLE",
+			director: "Susumu Mitsunaka",
+		},
+		{
+			title: "GOODBYE JULIA",
+			director: "Mohamed Kordofani",
+		},
+		{
+			title: "INCEPTION",
+			director: "Christopher Nolan",
+		},
+		{
+			title: "PARASITE",
+			director: "Bong Joon-ho",
+		},
+	];
 
 	const cinema2Name = "UGC Toison d'Or";
-	const movie3 = {
-		title: "THE WATCHERS",
-		director: "Ishana Night Shyamalan",
-	};
-	const movie4 = {
-		title: "BAD BOYS: RIDE OR DIE",
-		director: "Adil El Arbi, Bilall Fallah",
-	};
+
+	const moviesCinema2 = [
+		{
+			title: "THE WATCHERS",
+			director: "Ishana Night Shyamalan",
+		},
+		{
+			title: "BAD BOYS: RIDE OR DIE",
+			director: "Adil El Arbi, Bilall Fallah",
+		},
+		{
+			title: "TENET",
+			director: "Christopher Nolan",
+		},
+		{
+			title: "THE IRISHMAN",
+			director: "Martin Scorsese",
+		},
+	];
+
 
 	return (
 		<div>
 			<PageTitle title={pageTitle} />
 
-			<Cinema name={cinema1Name} movie1={movie1} movie2={movie2} />
+			<Cinema name={cinema1Name} movies={moviesCinema1} />
 
-			<Cinema name={cinema2Name} movie1={movie3} movie2={movie4} />
+			<Cinema name={cinema2Name} movies={moviesCinema2} />
 		</div>
 	);
 };
-
 
 export default App;
