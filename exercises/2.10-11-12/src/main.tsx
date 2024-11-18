ace96f02e67a23899a8f2619c7113cfe9631d682
Fétu Thimoté <lelemmingadorable@gmail.com>
2024-11-18 14:53:19 +0100

    new:ex2.12

--- exercises/2.10-11-12/src/main.tsx
+++ exercises/2.10-11-12/src/main.tsx
@@ -8,6 +8,7 @@ import App from "./components/App";
 import CinemaPage from "./components/pages/CinemaPage";
 import MovieListPage from "./components/pages/MovieListPage";
 import AddMoviePage from "./components/pages/AddMoviePage";
+import MoviePage from "./components/pages/MoviePage";
 
 const router = createBrowserRouter([
   {
@@ -29,6 +30,10 @@ const router = createBrowserRouter([
       {
         path: "add-movie",
         element: <AddMoviePage />,
+      },
+      {
+        path: "movies/:id",
+        element: <MoviePage />,
       },
     ],
   },
